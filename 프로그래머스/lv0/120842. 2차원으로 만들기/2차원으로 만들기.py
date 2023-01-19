b262b93ed4838ecdcba0cca5d29ce9564b0eff68
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-01-19 17:14:47 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,9 +1,5 @@
 def solution(num_list, n):
     answer = []
-    temp = []
-    for num in num_list:
-        temp.append(num)
-        if len(temp) == n:
-            answer.append(temp)
-            temp = []
+    for i in range(0, len(num_list), n):
+        answer.append(num_list[i:i+n])
     return answer
