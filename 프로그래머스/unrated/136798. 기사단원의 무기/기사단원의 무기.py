2ebdcd33bb0146c799c15db731586637d6ad289b
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-01-20 14:10:37 +0900

    [unrated] Title: 기사단원의 무기, Time: 2.07 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,20 @@
+def get_divisor(n):
+    divisor_list = []
+    for i in range(1, int(n**(1/2))+1):
+        if n % i == 0:
+            divisor_list.append(i)
+            if i ** 2 != n:
+                divisor_list.append(n // i)
+    divisor_list.sort()
+    return divisor_list
+
+
+def solution(number, limit, power):
+    answer = 0
+    for i in range(1, number+1):
+        attack = len(get_divisor(i))
+        if attack > limit:
+            answer += power
+        else:
+            answer += attack
+    return answer
