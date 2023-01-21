0f593f7488f15ca24c946953775a3379e25f5470
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-01-21 21:26:40 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,12 @@
+def get_divisor(n):
+    divisor_list = []
+    for i in range(1, int(n**(1/2)) + 1):
+        if n % i == 0:
+            divisor_list.append(i)
+            if i**2 != n:
+                divisor_list.append(n//i)
+    divisor_list.sort()
+    return divisor_list
+
+def solution(n):
+    return get_divisor(n)
