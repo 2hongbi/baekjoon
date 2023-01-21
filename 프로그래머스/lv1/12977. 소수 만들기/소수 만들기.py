b81815175debf445a8f1fd7dbc5400bc56636e70
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-01-21 22:00:20 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,17 @@
+def is_sosu(n):
+    for i in range(2, int(n**(1/2))+1):
+        if n % i == 0:
+            return False
+    return True
+            
+
+def solution(nums):
+    answer = 0
+    for i in range(len(nums)-2):
+        for j in range(i+1, len(nums)-1):
+            for k in range(j+1, len(nums)):
+                total = nums[i] + nums[j] + nums[k]
+
+                if is_sosu(total):
+                    answer += 1
+    return answer
