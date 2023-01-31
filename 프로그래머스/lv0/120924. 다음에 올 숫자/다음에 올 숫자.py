3b575a61ef78b6d07d4d04f9ab41f36c7c6ae626
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-01-31 22:57:51 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -0,0 +1,10 @@
+def solution(common):
+    seq = [y - x for x, y in zip(common, common[1:])]
+    
+    if len(set(seq)) == 1:
+        return common[0] + seq[0] * len(common)
+    else:
+        gongbi = seq[1] // seq[0]
+        return common[0] * (gongbi ** len(common))
+    answer = 0
+    return answer
