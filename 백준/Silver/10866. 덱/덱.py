393629a2e9f203808eabdd14707e75a7135c4118
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-03-01 18:13:21 +0900

    [Silver IV] Title: 덱, Time: 80 ms, Memory: 34160 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,25 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+deque = deque()
+
+for _ in range(n):
+    cmd = input().split()
+    if cmd[0] == 'push_front':
+        deque.appendleft(cmd[1])
+    elif cmd[0] == 'push_back':
+        deque.append(cmd[1])
+    elif cmd[0] == 'pop_front':
+        print(deque.popleft()) if deque else print(-1)
+    elif cmd[0] == 'pop_back':
+        print(deque.pop()) if deque else print(-1)
+    elif cmd[0] == 'size':
+        print(len(deque))
+    elif cmd[0] == 'empty':
+        print(0) if deque else print(1)
+    elif cmd[0] == 'front':
+        print(deque[0]) if deque else print(-1)
+    elif cmd[0] == 'back':
+        print(deque[-1]) if deque else print(-1)
