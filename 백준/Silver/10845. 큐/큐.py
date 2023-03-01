e04e8cb2ae64674d35d70173ab85cfadcd457aee
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-03-01 18:06:45 +0900

    [Silver IV] Title: 큐, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+stack = []
+
+for _ in range(n):
+    order = input().split()
+    if order[0] == 'push':
+        stack.append(order[1])
+    elif order[0] == 'pop':
+        print(stack.pop(0)) if stack else print(-1)
+    elif order[0] == 'size':
+        print(len(stack))
+    elif order[0] == 'empty':
+        print(0) if stack else print(1)
+    elif order[0] == 'front':
+        print(stack[0]) if stack else print(-1)
+    elif order[0] == 'back':
+        print(stack[-1]) if stack else print(-1)
