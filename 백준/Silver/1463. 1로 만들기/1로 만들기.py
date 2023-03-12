290c88bebc6a21579779baeccd81147d76b32158
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-03-12 20:31:16 +0900

    [Silver III] Title: 1로 만들기, Time: 560 ms, Memory: 39068 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,11 +1,15 @@
-X = int(input())
-dp = [0] * (X + 1)  # dp = 각 인덱스의 숫자가 1이 되는데 필요한 연산의 최솟값
+import sys
+input = sys.stdin.readline
 
-for i in range(2, X+1):
-    dp[i] = dp[i - 1] + 1   # 1을 뺴는 연산 1회 진행
+count = 0
+X = int(input())
+
+dp = [0] * (X + 1)  # i에서 1로 만드는 데 걸리는 최소 연간 개수
+for i in range(2, X + 1):
+    dp[i] = dp[i - 1] + 1  # 1을 빼는 연산 먼저 -> 1 연산을 한 셈으로 개수 카운트
     if i % 2 == 0:
         dp[i] = min(dp[i], dp[i // 2] + 1)
-    if i % 3 == 0:
+    if i % 3 == 0:  # 처음에는 if ~ elif 로 했는데 한꺼번에 2, 3 나누기 처리를 해도 되는 것
         dp[i] = min(dp[i], dp[i // 3] + 1)
 
 print(dp[X])
