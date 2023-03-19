2fdf2b63accc7f3b688777459af272e810069669
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-03-19 19:13:56 +0900

    [Bronze V] Title: 영수증, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+x = int(input())
+n = int(input())
+
+ans = 0
+for _ in range(n):
+    a, b = map(int, input().split())
+    ans += a * b
+
+if ans == x:
+    print('Yes')
+else:
+    print('No')
