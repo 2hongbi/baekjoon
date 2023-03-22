20be5ac1cf2beb4c7dbd49c284a21ee9d01fe302
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-03-22 20:42:20 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 126.02 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -0,0 +1,38 @@
+def solution(rows, columns, queries):
+    answer = []
+    matrix = [[0 for _ in range(columns + 1)] for _ in range(rows + 1)]
+    num = 1
+
+    for row in range(1, rows + 1):
+        for col in range(1, columns + 1):
+            matrix[row][col] = num
+            num += 1
+
+    for x1, y1, x2, y2 in queries:
+        temp = matrix[x1][y1]
+        minimum = temp
+
+        for k in range(x1, x2):
+            t = matrix[k + 1][y1]
+            matrix[k][y1] = t
+            minimum = min(minimum, t)
+
+        for k in range(y1, y2):
+            t = matrix[x2][k + 1]
+            matrix[x2][k] = t
+            minimum = min(minimum, t)
+
+        for k in range(x2, x1, -1):
+            t = matrix[k - 1][y2]
+            matrix[k][y2] = t
+            minimum = min(minimum, t)
+
+        for k in range(y2, y1, -1):
+            t = matrix[x1][k - 1]
+            matrix[x1][k] = t
+            minimum = min(minimum, t)
+
+        matrix[x1][y1 + 1] = temp
+        answer.append(minimum)
+
+    return answer
