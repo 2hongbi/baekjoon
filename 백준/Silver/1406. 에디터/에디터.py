711181518b425e34bae9756a5182f76f56709103
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-04-02 14:57:05 +0900

    [Silver II] Title: 에디터, Time: 264 ms, Memory: 41676 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -5,15 +5,15 @@ stack_r = []
 n = int(input())
 
 for i in range(n):
-    command = sys.stdin.readline().split()
+    cmd = sys.stdin.readline().split()
 
-    if command[0] == "L" and stack_l:
+    if cmd[0] == 'L' and stack_l:
         stack_r.append(stack_l.pop())
-    elif command[0] == "D" and stack_r:
+    elif cmd[0] == 'D' and stack_r:
         stack_l.append(stack_r.pop())
-    elif command[0] == "B" and stack_l:
+    elif cmd[0] == 'B' and stack_l:
         stack_l.pop()
-    elif command[0] == "P":
-        stack_l.append(command[1])
+    elif cmd[0] == 'P':
+        stack_l.append(cmd[1])
 
-print("".join(stack_l + list(reversed(stack_r))))
+print(''.join(stack_l + list(reversed(stack_r))))
