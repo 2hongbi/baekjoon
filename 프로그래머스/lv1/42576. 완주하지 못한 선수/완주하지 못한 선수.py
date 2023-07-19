e06d677236b54fe640ae5298a2bb2bfaf456887a
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-07-19 12:33:06 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 62.16 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,5 +1,6 @@
-from collections import Counter
+from collections import Counter # 데이터 셀 때 유용
 
+# dictionary가 list보다 최소 수십 배 이상 빠름
 def solution(participant, completion):
     answer = Counter(participant) - Counter(completion)
     return list(answer.keys())[0]
