337979f1f0aeb4a2775b323337871a3bff0ce9ee
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-07-21 13:10:55 +0900

    [level 1] Title: 모의고사, Time: 3.34 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -1,6 +1,6 @@
 def solution(answers):
     answer = []
-    score = [0, 0, 0]
+    scores = [0, 0, 0]
 
     student1 = [1, 2, 3, 4, 5]
     student2 = [2, 1, 2, 3, 2, 4, 2, 5]
@@ -8,15 +8,13 @@ def solution(answers):
     
     for idx, ans in enumerate(answers):
         if ans == student1[idx % len(student1)]:
-            score[0] += 1
+            scores[0] += 1
         if ans == student2[idx % len(student2)]:
-            score[1] += 1
+            scores[1] += 1
         if ans == student3[idx % len(student3)]:
-            score[2] += 1
+            scores[2] += 1
         
-    max_score = max(score)
-    for i in range(len(score)):
-        if score[i] == max_score:
-            answer.append(i + 1)
+    max_score = max(scores)
+    answer = [idx + 1 for idx, score in enumerate(scores) if score == max_score] 
     
     return answer
