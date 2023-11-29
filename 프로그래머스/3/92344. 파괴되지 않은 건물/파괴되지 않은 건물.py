be182c392206bc3d99a8766eb21fb0c9ea9cf981
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2023-11-29 15:47:31 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 626.97 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,26 +1,28 @@
 def solution(board, skill):
-    visited = [[0] * (len(board[0]) + 1) for _ in range(len(board) + 1)]
+    n, m = len(board), len(board[0])
+    visited = [[0] * (m + 1) for _ in range(n + 1)]
     
-    # type, (r1, c1)~(r2, c2) 범위, degree
     for t, r1, c1, r2, c2, degree in skill:
-        visited[r1][c1] += degree if t==2 else (-1)*degree
-        visited[r1][c2+1] += degree if t==1 else (-1)*degree
-        visited[r2+1][c1] += degree if t==1 else (-1)*degree
-        visited[r2+1][c2+1] += degree if t==2 else (-1)*degree
+        degree = degree if t == 2 else -degree
+        visited[r1][c1] += degree
+        visited[r1][c2 + 1] -= degree
+        visited[r2 + 1][c1] -= degree
+        visited[r2 + 1][c2 + 1] += degree
     
-    for j in range(len(board[0])):
-        for i in range(len(board)):
-            visited[i+1][j] += visited[i][j]
-        
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            visited[i][j+1] += visited[i][j]
+    # 가로 방향 누적합
+    for i in range(n):
+        for j in range(1, m):
+            visited[i][j] += visited[i][j - 1]
+
+    # 세로 방향 누적합
+    for j in range(m):
+        for i in range(1, n):
+            visited[i][j] += visited[i - 1][j]
     
     answer = 0
-    
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            board[i][j] += visited[i][j]
-            if board[i][j] > 0: answer += 1
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] + visited[i][j] > 0:
+                answer += 1
             
     return answer
