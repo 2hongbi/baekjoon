a73d3c3d4d45c473244010629df2af08cefdd76a
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-02-06 16:42:15 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 52 ms, Memory: 31132 KB -BaekjoonHub

---
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+battlefield = [input() for _ in range(m)]
+visited = [[False] * n for _ in range(m)]
+
+def dfs(x, y, team):
+    if x < 0 or x >= m or y < 0 or y >= n or visited[x][y] or battlefield[x][y] != team:
+        return 0
+    visited[x][y] = True
+    size = 1  # 현재 병사 포함
+    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        size += dfs(x + dx, y + dy, team)
+    return size
+
+power_w, power_b = 0, 0
+for i in range(m):
+    for j in range(n):
+        if not visited[i][j]:
+            if battlefield[i][j] == 'W':
+                power_w += dfs(i, j, 'W') ** 2
+            elif battlefield[i][j] == 'B':
+                power_b += dfs(i, j, 'B') ** 2
+
+print(power_w, power_b)
