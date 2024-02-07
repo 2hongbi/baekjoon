c6fd58640512b0556afd26c6a4d830e479a5401f
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-02-07 11:10:42 +0900

    [Silver III] Title: 1로 만들기, Time: 368 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,15 +1,17 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
 
-x = int(input())
+def make_one(x):
+    dp = [0] * (x + 1)
 
-dp = [0] * (x + 1)
+    for i in range(2, x + 1):
+        dp[i] = dp[i - 1] + 1
 
-for i in range(2, x + 1):
-    dp[i] = dp[i - 1] + 1
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i // 2] + 1)
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i // 3] + 1)
- 
-print(dp[x])
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+
+    return dp[x]
+
+
+print(make_one(n))
