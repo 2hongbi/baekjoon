634d0fe598df236ca5587caf112c615705b04425
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-03-23 17:38:22 +0900

    [Silver I] Title: 회의실 배정, Time: 4216 ms, Memory: 51900 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,18 +1,14 @@
 n = int(input())
+meetings = [tuple(map(int, input().split())) for _ in range(n)]
 
-timeline = []
-for _ in range(n):
-    start, end = map(int, input().split())
-    timeline.append([end, start]) # 종료 시각 우선 정렬이 먼저 -> 0번째에 종료 시각 먼저 작성
+meetings.sort(key=lambda x: (x[1], x[0]))
 
-timeline.sort()
+count = 1
+end_time = meetings[0][1]
 
-cnt = 0
-end = -1
+for i in range(1, n):
+    if meetings[i][0] >= end_time: # 이전 회의가 끝나는 시간 이후에 시작되는 시간 선택
+        count += 1
+        end_time = meetings[i][1]
 
-for i in range(n):
-    if timeline[i][1] >= end: # 겹치지 않은 다음 회의
-        end = timeline[i][0]
-        cnt += 1
-
-print(cnt)
+print(count)
