4531c0c7ea75ac1471b56e869c4ead9bf44e5ec0
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-03-23 17:17:45 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,18 +1,29 @@
 import sys
+
 input = sys.stdin.readline
 
 n = int(input())
+
 stack = []
 
 for _ in range(n):
-    order = input().split()
-    if order[0] == 'push':
-        stack.append(order[1])
-    elif order[0] == 'pop':
-        print(stack.pop()) if stack else print(-1)
-    elif order[0] == 'size':
+    cmd = input().strip().split()
+    if cmd[0] == 'push':
+        stack.append(int(cmd[1]))
+    elif cmd[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
         print(len(stack))
-    elif order[0] == 'empty':
-        print(0) if stack else print(1)
-    elif order[0] == 'top':
-        print(stack[-1]) if stack else print(-1)
+    elif cmd[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
