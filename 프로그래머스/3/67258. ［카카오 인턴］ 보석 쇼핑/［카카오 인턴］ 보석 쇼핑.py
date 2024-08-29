0853a61d0226949aaf8477b114a4978a2391f1d7
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-08-29 18:14:27 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 48.44 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,25 +1,23 @@
 def solution(gems):
-    gem_types = len(set(gems))  # 보석 종류의 수
+    gem_types = len(set(gems))
     gem_dict = {}
-    left = 0  # 왼쪽 포인터
-    min_length = len(gems) + 1  # 최소 구간 길이
-    answer = [0, len(gems) - 1]  # 최소 구간의 시작과 끝 인덱스
+    left = 0
+    min_length = len(gems) + 1 # 최소 구간 길이
+    answer = [0, len(gems) - 1]
     
     for right in range(len(gems)):
-        # 오른쪽 포인터가 가리키는 보석을 추가
+        # 오른쪽 포인터가 가리키는 보석 추가
         if gems[right] in gem_dict:
             gem_dict[gems[right]] += 1
         else:
             gem_dict[gems[right]] = 1
         
-        # 모든 보석을 포함하는 구간인지 확인
         while len(gem_dict) == gem_types:
-            # 현재 구간이 최소 구간인지 확인
             if right - left < min_length:
                 min_length = right - left
-                answer = [left + 1, right + 1] 
-            
-            # 왼쪽 포인터를 이동시켜 구간을 좁힘
+                answer = [left + 1, right + 1]
+
+            # 구간 좁히기
             gem_dict[gems[left]] -= 1
             if gem_dict[gems[left]] == 0:
                 del gem_dict[gems[left]]
