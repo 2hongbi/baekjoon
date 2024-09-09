8284c4101a0788201b8a19fd3ce0d4400ca744e7
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-09-10 01:05:44 +0900

    [Silver IV] Title: 카드2, Time: 180 ms, Memory: 51068 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,15 +1,10 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
 
 n = int(input())
+queue = deque(range(1, n+1))
 
-cards = deque()
-for i in range(1, n + 1):
-    cards.append(i)
+while len(queue) > 1:
+    queue.popleft()
+    queue.append(queue.popleft())
 
-while len(cards) > 1:
-    cards.popleft()
-    cards.append(cards.popleft())
-
-print(cards[0])
+print(queue[0])
