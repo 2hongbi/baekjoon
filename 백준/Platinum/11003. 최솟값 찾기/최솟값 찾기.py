a9d011f1e9ca403e2feb0999a231a769a09819f9
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-09-09 23:40:42 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 5960 ms, Memory: 623280 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -3,14 +3,19 @@ from collections import deque
 
 input = sys.stdin.readline
 
-N, L = map(int, input().split())
-deque = deque()
-A = list(map(int, input().split()))
+n, l = map(int, input().split())
+a = list(map(int, input().split()))
 
-for i in range(N):
-    while deque and deque[-1][0] > A[i]:
-        deque.pop()
-    deque.append((A[i], i))
-    if deque[0][1] <= i - L:  # 범위에서 벗어난 값은 덱에서 제거
-        deque.popleft()
-    print(deque[0][0], end=' ')
+queue = deque() # 슬라이딩 윈도우에서 최소값의 인덱스를 저장하는 덱
+
+for i in range(n):
+    # 현재 값보다 큰 값들은 덱에서 제거
+    while queue and a[queue[-1]] > a[i]:
+        queue.pop() # 오른쪽 끝의 요소 제거
+
+    queue.append(i) # 현재 인덱스를 덱에 추가
+
+    if queue[0] <= i - l:
+        queue.popleft() # 왼쪽 끝의 요소 제거
+
+    print(a[queue[0]], end=' ')
