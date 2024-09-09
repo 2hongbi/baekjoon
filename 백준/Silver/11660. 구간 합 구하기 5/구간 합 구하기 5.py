3ff4a67b563ebf0f2cb4721b4c3e824d48dd677a
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-09-09 17:23:36 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 944 ms, Memory: 105388 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,17 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-A = [[0] * (N + 1)]     # 원본 리스트
-D = [[0] * (N + 1) for _ in range(N + 1)]   # 합 배열
+n, m = map(int, input().split())
 
-for _ in range(N):
-    A.append([0] + [int(x) for x in input().split()])
+# 원본 배열 입력 받기
+arr = [[0] * (n + 1)]
+for i in range(n):
+    arr.append([0] + list(map(int, input().split())))
 
-for i in range(1, N + 1):
-    for j in range(1, N + 1):
-        D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j]
+# 누적합 배열 초기화
+prefix_sum = [[0] * (n + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        prefix_sum[i][j] = prefix_sum[i-1][j] + prefix_sum[i][j-1] - prefix_sum[i-1][j-1] + arr[i][j]
 
-for _ in range(M):
+# 누적합 출력
+for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    print(D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1])
+    print(prefix_sum[x2][y2] - prefix_sum[x1-1][y2] - prefix_sum[x2][y1-1] + prefix_sum[x1-1][y1-1])
