51cdbceef42b158c366749f2ba2f3586cf980f79
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-09-12 23:18:09 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -8,8 +8,11 @@ grid = [
 
 visited = [[False] * m for _ in range(n)]
 
+dxs, dys = [1, 0, -1, 0], [0, 1, 0, -1]
+
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < m
+
 
 def can_go(x, y):
     if not in_range(x, y):
@@ -18,18 +21,21 @@ def can_go(x, y):
         return False
     return True
 
+
 def bfs(x, y):
     queue = deque([(x, y)])
     visited[x][y] = True
 
     while queue:
         x, y = queue.popleft()
-        for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+
+        for dx, dy in zip(dxs, dys):
             nx, ny = x + dx, y + dy
             if can_go(nx, ny):
-                visited[nx][ny] = True
                 grid[nx][ny] = grid[x][y] + 1
                 queue.append((nx, ny))
+                visited[nx][ny] = True
+
 
 bfs(0, 0)
 print(grid[n - 1][m - 1])
