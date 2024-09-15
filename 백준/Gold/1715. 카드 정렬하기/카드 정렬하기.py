6f87e5a9444c933c043bd3cfa5ebaa73a02c3d17
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-09-15 23:39:25 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 2928 ms, Memory: 38024 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -1,18 +1,19 @@
 from queue import PriorityQueue
 
 n = int(input())
-pq = PriorityQueue()
 
+pq = PriorityQueue()
 for _ in range(n):
     pq.put(int(input()))
 
 data1, data2 = 0, 0
-sum = 0
+total = 0
 
 while pq.qsize() > 1:
-    data1, data2 = pq.get(), pq.get()
+    data1 = pq.get()
+    data2 = pq.get()
     temp = data1 + data2
-    sum += temp
+    total += temp
     pq.put(temp)
 
-print(sum)
+print(total)
