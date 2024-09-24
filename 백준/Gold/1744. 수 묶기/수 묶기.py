d47cc83e5c510c816bc73f0fa8af07645a546fd6
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-09-24 16:04:48 +0900

    [Gold IV] Title: 수 묶기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.py
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -1,45 +1,46 @@
-from queue import PriorityQueue
+import sys
+input = sys.stdin.readline
+
+# 양수, 음수, 1, 0 구분해서 리스트 저장
+# 각각의 리스트 정렬 > 양수는 내림차순 -> 큰 수끼리 묶기, 음수는 오름차순 정렬 -> 절댓값이 큰 수끼리
+# 1보다 큰 양수는 가능한 한 곱하고, 음수는 곱할 수 있을 만큼 묶기
 
 n = int(input())
+numbers = [int(input()) for _ in range(n)]
 
-plus_pq = PriorityQueue()
-minus_pq = PriorityQueue()
-one_cnt = 0
-zero_cnt = 0
-
-for _ in range(n):
-    num = int(input())
-
-    # 양수 내림차순 정렬을 위해 -1을 곱하여 저장
-    if num > 1:
-        plus_pq.put(num * -1)
-    elif num < 0:
-        minus_pq.put(num)
-    elif num == 1:
-        one_cnt += 1
-    elif num == 0:
-        zero_cnt += 1
-
+positive, negative = [], []
+zeros, ones = 0, 0
 
 result = 0
 
-while plus_pq.qsize() > 1:
-    data1 = plus_pq.get() * -1
-    data2 = plus_pq.get() * -1
-    result += data1 * data2
+for num in numbers:
+    if num > 1:
+        positive.append(num)
+    elif num == 0:
+        zeros += 1
+    elif num == 1:
+        ones += 1
+    else:
+        negative.append(num)
 
-if plus_pq.qsize() > 0:
-    result += plus_pq.get() * -1
+positive.sort(reverse=True) # 내림차순 정렬
+negative.sort()
 
-while minus_pq.qsize() > 1:
-    data1 = minus_pq.get()
-    data2 = minus_pq.get()
-    result += data1 * data2
+# 양수 두 개씩 묶어 곱하기
+for i in range(0, len(positive) - 1, 2):
+    result += positive[i] * positive[i + 1]
+# 남은 양수가 있으면 더하기
+if len(positive) % 2 == 1:
+    result += positive[-1]
 
-if minus_pq.qsize() > 0:
-    if zero_cnt == 0:
-        result += minus_pq.get()
+# 음수 두 개씩 묶어 곱하기
+for i in range(0, len(negative) - 1, 2):
+    result += negative[i] * negative[i + 1]
+# 음수가 홀수 개 남았을 때 0이 있으면 묶어서 없애기
+if len(negative) % 2 == 1 and zeros == 0:
+    result += negative[-1]
 
-result += one_cnt
+# 1은 더하기
+result += ones
 
 print(result)
