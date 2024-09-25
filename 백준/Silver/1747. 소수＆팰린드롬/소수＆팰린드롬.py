c441e56db7db202e7df099b3416408a8cd10e8b0
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-09-25 12:02:39 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 360 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -1,28 +1,26 @@
-# 소수 & 팰린드롬
-import sys
 import math
 
-input = sys.stdin.readline
 
-
+# 소수 판별
 def is_prime(x):
-    if x == 1:
+    if x < 2:
         return False
+
     for i in range(2, int(math.sqrt(x)) + 1):
         if x % i == 0:
             return False
     return True
 
 
+# 팰린드롬 판별
 def is_palindrome(x):
-    str_x = str(x)
-    return str_x == str_x[::-1]
+    return str(x) == str(x)[::-1]
 
 
 n = int(input())
 
-while n <= 1050000:
-    if is_prime(n) and is_palindrome(n):
+while True:
+    if is_palindrome(n) and is_prime(n):
         print(n)
         break
     n += 1
