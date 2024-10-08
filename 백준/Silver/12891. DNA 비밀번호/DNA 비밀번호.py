ca1760d45bf60cf409ce6aa16fd90fb5828adc8b
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-10-08 18:38:12 +0900

    [Silver II] Title: DNA 비밀번호, Time: 576 ms, Memory: 34580 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -1,10 +1,12 @@
+import sys
 from collections import Counter
 
-s, p = map(int, input().split()) # dna 문자열 길이, 비밀번호로 사용할 부분문자열의 길이
-dna_string = input()
-min_count = list(map(int, input().split())) # 부분 문자열에 포함되어야 할 A, C, G, T의 최소 개수
+input = sys.stdin.readline
 
-# 현재 윈도우 내의 각 문자 빈도수 카운터
+s, p = map(int, input().split())
+dna_string = input().strip()
+min_count = list(map(int, input().split())) # minimum count of a, c, g, t
+
 current_count = Counter(dna_string[:p])
 
 def is_valid():
@@ -13,21 +15,20 @@ def is_valid():
             current_count['G'] >= min_count[2] and
             current_count['T'] >= min_count[3])
 
+answer = 0
 
-valid_password = 0
-# 첫 번째 윈도우 검사
 if is_valid():
-    valid_password += 1
+    answer += 1
+
 
 for i in range(1, s - p + 1):
     prev_char = dna_string[i - 1]
-    current_count[prev_char] -= 1
+    next_char = dna_string[i + p - 1]
 
-    # 새문자 추가
-    new_char = dna_string[i + p - 1]
-    current_count[new_char] += 1
+    current_count[prev_char] -= 1
+    current_count[next_char] += 1
 
     if is_valid():
-        valid_password += 1
+        answer += 1
 
-print(valid_password)
+print(answer)
