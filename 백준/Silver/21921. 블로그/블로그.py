2568b1c707db2423dec14fb0ca92b4c21a597696
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-10-10 11:00:52 +0900

    [Silver III] Title: 블로그, Time: 128 ms, Memory: 58780 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -1,10 +1,9 @@
 n, x = map(int, input().split()) # 블로그를 시작하고 지난 일수, X일 동안
 visitors = list(map(int, input().split()))
 
-# 첫 x일 동안의 방문자 수의 합을 초기값으로 설정: 슬라이딩 윈도우
 current_sum = sum(visitors[:x])
 max_visitors = current_sum
-count = 1 # 기간
+count = 1
 
 for i in range(x, n):
     current_sum += visitors[i] - visitors[i - x]
@@ -16,8 +15,4 @@ for i in range(x, n):
         count += 1
 
 
-if max_visitors == 0:
-    print("SAD")
-else:
-    print(max_visitors)
-    print(count)
+print(max_visitors, count, sep='\n') if max_visitors else print("SAD")
