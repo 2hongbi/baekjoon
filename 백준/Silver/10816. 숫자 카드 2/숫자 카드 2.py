547829309190a873a249f813557ad072e10b3133
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-10-10 11:07:04 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 692 ms, Memory: 137228 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -3,19 +3,16 @@ from collections import defaultdict
 input = sys.stdin.readline
 
 n = int(input())
-card_dict = defaultdict(int)
+cards = list(map(int, input().split()))
 
-tmp = list(map(int, input().split()))
-for t in tmp:
-    card_dict[t] += 1
+card_dict = defaultdict(int)
+for card in cards:
+    card_dict[card] += 1
 
 m = int(input())
-check_list = list(map(int, input().split()))
-tmp = []
-for check in check_list:
-    if check in card_dict.keys():
-        tmp.append(str(card_dict[check]))
-    else:
-        tmp.append('0')
+targets = list(map(int, input().split()))
+answer = []
+for target in targets:
+    answer.append(card_dict.get(target, 0))
 
-print(' '.join(tmp))
+print(*answer)
