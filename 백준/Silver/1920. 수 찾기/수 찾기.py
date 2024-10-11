00ffe4ba83f1287b7166e85013dfadf9f1c7a4e4
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-10-11 21:20:21 +0900

    [Silver IV] Title: 수 찾기, Time: 160 ms, Memory: 50540 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -2,26 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-numbers = list(map(int, input().split()))
+numbers = set(map(int, input().split()))
+
 m = int(input())
 targets = list(map(int, input().split()))
 
-numbers.sort()
-
-def binary_search(arr, x):
-    start, end = 0, len(arr) - 1
-
-    while start <= end:
-        mid = (start + end) // 2
-
-        if arr[mid] == x:
-            return True
-        elif arr[mid] < x:
-            start = mid + 1
-        else:
-            end = mid - 1
-    else:
-        return False
-
 for t in targets:
-    print(1) if binary_search(numbers, t) else print(0)
+    if t in numbers:
+        print(1)
+    else:
+        print(0)
