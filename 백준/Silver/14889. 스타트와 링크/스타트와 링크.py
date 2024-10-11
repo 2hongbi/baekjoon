fe74ee56b9c3336ccc294273736b8730a8efbc5a
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-10-11 22:58:30 +0900

    [Silver I] Title: 스타트와 링크, Time: 4736 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,30 +1,35 @@
 import sys
-from itertools import combinations
 input = sys.stdin.readline
 
 n = int(input())
 s = [list(map(int, input().split())) for _ in range(n)]
 
 min_diff = float('inf')
+team = [False] * n  # False는 링크 팀, True는 스타트팀
 
-people = [i for i in range(n)]
-for start_team in combinations(people, n // 2):
-    # print(start_team)
-    link_team = [i for i in people if i not in start_team]
-
-    start_score = 0
-    for i in start_team:
-        for j in start_team:
-            if i != j:
+def calculate_team_score():
+    start_score, link_score = 0, 0
+    for i in range(n):
+        for j in range(n):
+            if team[i] and team[j]:
                 start_score += s[i][j]
-
-    link_score = 0
-    for i in link_team:
-        for j in link_team:
-            if i != j:
+            elif not team[i] and not team[j]:
                 link_score += s[i][j]
+    return abs(start_score - link_score)
 
-    diff = abs(start_score - link_score)
-    min_diff = min(min_diff, diff)
+def backtrack(depth, idx):
+    global min_diff
 
+    if depth == n // 2:
+        min_diff = min(min_diff, calculate_team_score())
+        return
+
+    for i in range(idx, n):
+        if not team[i]:
+            team[i] = True # 스타트 팀 배정
+            backtrack(depth + 1, i + 1)
+            team[i] = False
+
+
+backtrack(0, 0)
 print(min_diff)
