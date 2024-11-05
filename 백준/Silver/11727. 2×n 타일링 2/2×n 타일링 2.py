62d9fd3850092da9b62b8e6269aeba37c11c5605
Soyeon Lee <79040336+2hongbi@users.noreply.github.com>
2024-11-05 18:33:04 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,9 +1,15 @@
-import sys
-input = sys.stdin.readline
+def solution(n):
+    MOD = 10007
 
-s = [0, 1, 3]
-for i in range(3, 1001):
-    s.append((s[i-2] * 2 + s[i-1]) % 10007)
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    if n >= 2:
+        dp[2] = 3
+
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2] * 2) % MOD
+
+    return dp[n]
 
 n = int(input())
-print(s[n])
+print(solution(n))
